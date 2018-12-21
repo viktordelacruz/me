e40c54d255ebf52b1a64c1c8d1eb000a846b411a
Viktor Dela Cruz <viktormdelacruz@gmail.com>
2018-12-21 16:44:15 +0800

    Updates

---
+++ precache-manifest.4bcbd87c5b46d84ac4007a6ecd0d9b70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4ae667e0f9864d95541",
+    "url": "/me/static/js/runtime~main.f4ae667e.js"
+  },
+  {
+    "revision": "288a2265194f161d42ed",
+    "url": "/me/static/js/main.288a2265.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/me/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "288a2265194f161d42ed",
+    "url": "/me/static/css/main.41bdd228.chunk.css"
+  },
+  {
+    "revision": "7f858a34a998edd514601aa1b06b7fb1",
+    "url": "/me/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/me/precache-manifest.74c7c25d9c44568f7aa1b45cf59ba4c8.js"
+  "/me/precache-manifest.4bcbd87c5b46d84ac4007a6ecd0d9b70.js"
 );
 
 workbox.clientsClaim();
