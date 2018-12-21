35628a97271806e819028b86fff74261f52b9cc7
Viktor Dela Cruz <viktormdelacruz@gmail.com>
2018-12-21 16:27:57 +0800

    Updates

---
+++ precache-manifest.74c7c25d9c44568f7aa1b45cf59ba4c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/me/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f4ae667e0f9864d95541",
+    "url": "/me/static/js/runtime~main.f4ae667e.js"
+  },
+  {
+    "revision": "4a3ef2aa36d273892e12",
+    "url": "/me/static/js/main.4a3ef2aa.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/me/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "4a3ef2aa36d273892e12",
+    "url": "/me/static/css/main.831f952c.chunk.css"
+  },
+  {
+    "revision": "2949a337632e0aae9ff4cfd795d3a1eb",
+    "url": "/me/index.html"
+  }
+];
